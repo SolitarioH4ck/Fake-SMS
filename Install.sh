c6c01d639a82ffbfc22c1b4ca53d3d46d2077d27
SolitarioH4ck <128333029+SolitarioH4ck@users.noreply.github.com>
2023-04-18 16:06:40 -0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,7 +2,7 @@ dr='\033[1;34m'
 rd='\033[1;30m'
 nv='\033[1;39m'
 
-echo -e "${dr}Instalando Dependencias...{nv}"
+echo -e "${dr}Instalando Dependencias...${nv}"
 pkg install openssl
 
 echo -e ${dr}"Instalando requests..${nv}"
